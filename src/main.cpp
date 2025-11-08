45a9996393ee84eb44cb8aa59e80b8db3d198667
Chrisgree <chrisgreen3215@gmail.com>
2025-11-08 00:35:13 -0600

    rfid-code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-
+#include <arduino.h>
 #include <SPI.h>
 #include <MFRC522.h>
 
@@ -7,6 +7,7 @@
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   
 
+//Starts communication with RFID reader and computer, prepares everything, and tells user to scan card
 void setup() {
   Serial.begin(9600);                                          
   SPI.begin();                                                  
@@ -16,81 +17,49 @@ void setup() {
 
 void loop() {
 
-  MFRC522::MIFARE_Key key;
-  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+  //Creates and sets the key to default key
+  MFRC522::MIFARE_Key key; 
+  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF; 
 
-  byte block;
-  byte len;
-  MFRC522::StatusCode status;
+  byte block; 
+  byte len; 
+  MFRC522::StatusCode status; //Track the status of the RFID 
 
+  //If no card then it restarts the loop
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
     return;
   }
 
+  //If there is a card then it reads its UID, else then it exits
   if ( ! mfrc522.PICC_ReadCardSerial()) {
     return;
   }
 
-  Serial.println(F("**Card Detected:**"));
-
+  Serial.println(F("**Card Detected:**")); 
   mfrc522.PICC_DumpDetailsToSerial(&(mfrc522.uid)); 
+  Serial.print(F("Name: ")); 
 
-  Serial.print(F("Name: "));
+  byte buffer1[18]; 
+  block = 4; 
+  len = 18; 
+  status = mfrc522.MIFARE_Read(block, buffer1, &len); //Reads data of MIFARE_Read and stores it into buffer1
 
-  byte buffer1[18];
-
-  block = 4;
-  len = 18;
-
-  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 4, &key, &(mfrc522.uid)); 
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("Authentication failed: "));
-    Serial.println(mfrc522.GetStatusCodeName(status));
-    return;
-  }
-
-  status = mfrc522.MIFARE_Read(block, buffer1, &len);
+  //If the card being read was failed then the code returns to the loop itself until the card was read successfully 
   if (status != MFRC522::STATUS_OK) {
     Serial.print(F("Reading failed: "));
     Serial.println(mfrc522.GetStatusCodeName(status));
     return;
   }
 
-  for (uint8_t i = 0; i < 16; i++)
-  {
-    if (buffer1[i] != 32)
-    {
-      Serial.write(buffer1[i]);
-    }
-  }
-  Serial.print(" ");
-
-
-  byte buffer2[18];
-  block = 1;
-
-  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 1, &key, &(mfrc522.uid)); 
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("Authentication failed: "));
-    Serial.println(mfrc522.GetStatusCodeName(status));
-    return;
-  }
-
-  status = mfrc522.MIFARE_Read(block, buffer2, &len);
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("Reading failed: "));
-    Serial.println(mfrc522.GetStatusCodeName(status));
-    return;
-  }
-
+  //If the card being read was a success then it goes to each byte from the card and prints the bytes to the serial monitor
   for (uint8_t i = 0; i < 16; i++) {
-    Serial.write(buffer2[i] );
+    Serial.write(buffer1[i] );
   }
 
-  Serial.println(F("\n**End Reading**\n"));
+  Serial.println(F("\n**End Reading**\n")); 
 
+  //Delays for 1 second and stops everything else 
   delay(1000); 
-
   mfrc522.PICC_HaltA();
   mfrc522.PCD_StopCrypto1();
 }
